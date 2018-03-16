0f079a5fcc115ec6434a68533597ad14a197849d
Kingmannie <simon.browne0121@hotmail.com>
2018-03-16 04:39:20 +0000

    items in listbox as objects are the arrays of each listbox instance.

--- Jukebox_Assignment_SB/Jukebox_Assignment_SB/frm_Jukebox.cs
+++ Jukebox_Assignment_SB/Jukebox_Assignment_SB/frm_Jukebox.cs
@@ -18,6 +18,12 @@ namespace jukebox_assignment_SB
             InitializeComponent();
         }
 
+        //initialise listbox array globally
+        ListBox[] Genre_List;
+
+        //empty string container
+        String next_line = "";
+
         public void frm_Jukebox_Load(object sender, EventArgs e)
         {
             //on form load, need to create the config file.
@@ -44,46 +50,61 @@ namespace jukebox_assignment_SB
             
             StreamReader myInputStream = File.OpenText(configPath + "Config.txt");
 
-            //empty string container
-            String next_line = "";
-
             //capture first line of data from the config file
             next_line = myInputStream.ReadLine();
 
+            String num_track = "";
+
             //create a new instance of listbox
             //array index value is converted string from config file.
-            ListBox[] Genre_List = new ListBox[Convert.ToInt16(next_line)];
+            Genre_List = new ListBox[Convert.ToInt16(next_line)];
 
             //loop to define genre - (3 genres as default) 
             for (int gen = 0; gen < Convert.ToInt16(next_line); gen++)
             {
                 //create new instance of Genre_List for each gen increment
                 Genre_List[Convert.ToInt16(gen)] = new ListBox();
-                next_line = myInputStream.ReadLine();//number of tracks in config
-                Genre_List[Convert.ToInt16(gen)].Text = myInputStream.ReadLine();//store title in array
-                //num_track = Append(myInputStream.ReadLine());
-                txt_Genre_Title.Text = Genre_List[gen].Text.ToString();
-
-                for (int track = 0; track < Convert.ToInt32(next_line); track++)
+                num_track = myInputStream.ReadLine();//number of tracks in config
+                Genre_List[Convert.ToInt16(gen)].Items.Add(myInputStream.ReadLine());//store title in array
+                //txt_Genre_Title.Text = Genre_List[gen].Text.ToString();
+                
+                for (int track = 0; track < Convert.ToInt32(num_track); track++)
                 { //loop to define tracks
-                    Genre_List[Convert.ToInt16(gen)].Text = myInputStream.ReadLine();//store tracks in array
-                    lst_Genre_List.Items[Convert.ToInt32(next_line)].Equals(Genre_List[track].Text.ToString());
+                    Genre_List[Convert.ToInt16(gen)].Items.Add(myInputStream.ReadLine());//store tracks in array
+                    //lst_Genre_List.Items[Convert.ToInt32(next_line)].Equals(Genre_List[track].Text.ToString());
                 }
             }
 
+            //gotcha
+            //txt_Genre_Title.Text = Genre_List[0].Items[1].ToString();
+
             //after populating the arrays - display on form
-            
+
             //lst_Genre_List
+            //txt_Genre_Title.Text
 
             myInputStream.Close();
         }
 
-        public void hscr_Selector_Scroll(object sender, ScrollEventArgs e)
+
+        private void hscr_Selector_Scroll(object sender, ScrollEventArgs e)
         {
+            string configPath = Directory.GetCurrentDirectory() + "\\";
 
+            StreamReader myInputStream = File.OpenText(configPath + "Config.txt");
 
+            
 
+            //txt_Genre_Title.Text = Genre_List[hscr_Selector.Value].Text.ToString();
+
+            //potentially search document here
+            //for (int i = 0; i < Convert.ToInt16(Genre_List[0]); i++)
+            //{
+            //hscr_Selector.Value += i;
+            //txt_Genre_Title.Text = Genre_List[hscr_Selector.Value].Text.ToString();
             //txt_Genre_Title.Text = hscr_Selector.Value.ToString();
+            //}
+            myInputStream.Close();
         }
 
         private void lst_Genre_List_SelectedIndexChanged(object sender, EventArgs e)
